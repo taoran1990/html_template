f3dbb4af56c6e5bc5208c4a566d234fdd0532a67
taorna1990 <1809147554@qq.com>
2018-03-19 16:39:55 +0800

    add three.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,15 @@
-console.log("lib");
-console.log("main");
+//console.log("lib");
+
+const Login 	 = { template: "<div>login</div>" }
+const Register = { template: "<div>register</div>" }
+const Main     = { template: "<div>main</div>" }
+
+const router = [
+	{ path: 'login'		, component: Login		},
+	{ path: 'register', component: Register	},
+	{ path: 'main'		, component: Main			}
+]
+
+window.onload = function(){
+	const app = new Vue({router}).$mount('#app');
+}
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var uglify = require('gulp-uglify');
 var concat = require("gulp-concat");
 
 gulp.task('serve', ['html', 'js', 'sass', 'watch'], function(){
-	var server = gls.static('docs', 4000); 
+	var server = gls.static('docs', 3000); 
 	server.start();
 
 
@@ -18,14 +18,14 @@ gulp.task('serve', ['html', 'js', 'sass', 'watch'], function(){
 
 function js(){
 	return gulp.src('src/js/**/*.js')
-				.pipe(uglify())
+//				.pipe(uglify())
 				.pipe(concat("main.js"))
 				.pipe(gulp.dest('docs/js'))
 }
 
 function html(){
-	return gulp.src('src/*.html').pipe(gulp.dest('docs/'));
-		gulp.src('src/html/*.html').pipe(gulp.dest('docs/html'));
+	gulp.src('src/*.html').pipe(gulp.dest('docs/'));
+	return gulp.src('src/html/*.html').pipe(gulp.dest('docs/html'));
 }
 
 function css(){
@@ -50,10 +50,6 @@ gulp.task('watch', function(){
 
 	gulp.watch('src/js/**/*.js', function(){
 		js();
-	});
-
-	gulp.watch('src/**/*.html', function(){
-		html();
 	});
 
 	gulp.watch('src/sass/**/*.scss', function(){
