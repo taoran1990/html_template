55000d5a28e227268bf42232f85016d317c2cff9
taorna1990 <1809147554@qq.com>
2018-03-19 11:47:07 +0800

    add watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var gls  = require('gulp-live-server');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require("gulp-concat");
 
-gulp.task('serve', ['html','sass'], function(){
-	var server = gls.static('dist', 4000); 
+gulp.task('serve', ['html', 'js', 'sass', 'watch'], function(){
+	var server = gls.static('docs', 4000); 
 	server.start();
 
 
@@ -14,15 +16,50 @@ gulp.task('serve', ['html','sass'], function(){
 
 });
 
-gulp.task('html', function(){
-	gulp.src('src/*.html').pipe(gulp.dest('docs/'));
-	gulp.src('src/html/*.html').pipe(gulp.dest('docs/html'));
-});
+function js(){
+	return gulp.src('src/js/**/*.js')
+				.pipe(uglify())
+				.pipe(concat("main.js"))
+				.pipe(gulp.dest('docs/js'))
+}
 
-gulp.task('sass', function(){
+function html(){
+	return gulp.src('src/*.html').pipe(gulp.dest('docs/'));
+		gulp.src('src/html/*.html').pipe(gulp.dest('docs/html'));
+}
+
+function css(){
 	return gulp.src('src/sass/**/*.scss')
 							.pipe(sass().on('error', sass.logError))
 							.pipe(gulp.dest('docs/css'));
+}
+
+gulp.task('js', function(){
+	js();
+});
+
+gulp.task('html', function(){
+	html();
+});
+
+gulp.task('sass', function(){
+	sass();
+})
+
+gulp.task('watch', function(){
+
+	gulp.watch('src/js/**/*.js', function(){
+		js();
+	});
+
+	gulp.watch('src/**/*.html', function(){
+		html();
+	});
+
+	gulp.watch('src/sass/**/*.scss', function(){
+		css();
+	});
+
 })
 
 
