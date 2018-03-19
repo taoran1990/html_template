73a918d9136c893cdf5fc8aa5ec4bba9de51fa17
taorna1990 <1809147554@qq.com>
2018-03-19 10:18:50 +0800

    add docs html

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var gls  = require('gulp-live-server');
 var sass = require('gulp-sass');
 
-gulp.task('serve', ['sass'], function(){
+gulp.task('serve', ['html','sass'], function(){
 	var server = gls.static('dist', 4000); 
 	server.start();
 
 
-	gulp.watch(['dist/*.html', 'dist/js/*.js', 'dist/css/*.css'], 
+	gulp.watch(['docs/*.html', 'docs/js/*.js', 'docs/css/*.css'], 
 		function(file){
 			server.notify.apply(server, [file]);
 	});
@@ -15,14 +15,14 @@ gulp.task('serve', ['sass'], function(){
 });
 
 gulp.task('html', function(){
-
+	gulp.src('src/*.html').pipe(gulp.dest('docs/'));
+	gulp.src('src/html/*.html').pipe(gulp.dest('docs/html'));
 });
 
 gulp.task('sass', function(){
 	return gulp.src('src/sass/**/*.scss')
 							.pipe(sass().on('error', sass.logError))
-							.pipe(gulp.dest('dist/css'));
+							.pipe(gulp.dest('docs/css'));
 })
-
 
 
