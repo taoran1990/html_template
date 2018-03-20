e1c3f90c9800489a5a98d83fd0101d8976a3b337
taorna1990 <1809147554@qq.com>
2018-03-20 10:17:02 +0800

    add login page

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,16 @@ var gls  = require('gulp-live-server');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var concat = require("gulp-concat");
+var server = require('gulp-server-livereload');
 
 gulp.task('serve', ['html', 'js', 'sass', 'watch'], function(){
 	var server = gls.static('docs', 3000); 
 	server.start();
 
-
 	gulp.watch(['docs/*.html', 'docs/js/*.js', 'docs/css/*.css'], 
 		function(file){
 			server.notify.apply(server, [file]);
 	});
-
 });
 
 function js(){
@@ -25,7 +24,7 @@ function js(){
 
 function html(){
 	gulp.src('src/*.html').pipe(gulp.dest('docs/'));
-	return gulp.src('src/html/*.html').pipe(gulp.dest('docs/html'));
+	return gulp.src('src/*.html').pipe(gulp.dest('docs/html'));
 }
 
 function css(){
@@ -50,6 +49,10 @@ gulp.task('watch', function(){
 
 	gulp.watch('src/js/**/*.js', function(){
 		js();
+	});
+
+	gulp.watch('src/*.html', function(){
+		html();
 	});
 
 	gulp.watch('src/sass/**/*.scss', function(){
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,1 @@
-const Login 	 = { template: "<div>login</div>" }
-const Register = { template: "<div>register</div>" }
-const Main     = { template: "<div>main</div>" }
 
-const router = [
-	{ path: 'login'		, component: Login		},
-	{ path: 'register', component: Register	},
-	{ path: 'main'		, component: Main			}
-]
-
-window.onload = function(){
-	const app = new Vue({router}).$mount('#app');
-}
